b21bba21e4b61d6daa44d6da09c5c6e8d46875ca
Sssakhi <cacayoussef5@gmail.com>
2023-09-27 09:13:11 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 /**
- * main - that prints the phrase
- * return:0 dimmman
+ * main - that prints the phrase with puts fonction
+ * return: 0 (success)
  */
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
