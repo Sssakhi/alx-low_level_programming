11e42ab0e35c80871726b3c69eb146cd0acb9076
Sssakhi <cacayoussef5@gmail.com>
2023-10-03 10:55:25 -0700

    0x0A. C - argc, argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - fonction that print its name
+ *
+ * Return: Always 0 (success)
+ */
+int main(int argc , char const *argv[])
+{
+	(void)argc;
+	printf("%s/n", argv[0]);
+return (0);
+}
