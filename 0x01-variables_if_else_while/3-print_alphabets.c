6e185988c2ebecf7f738c12f17556628dc512682
Sssakhi <cacayoussef5@gmail.com>
2023-10-05 08:19:12 -0700

    d

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,11 +6,13 @@
  */
 int main(void)
 {
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
-	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	for(i = 0; i < 52; i++)
+
+	for (i = 0; i < 52; i++)
 	{
-		putchar(alph[i]);
+		putchar(alp[i]);
 	}
+	putchar('\n');
 	return (0);
 }
